e12f203a8fd1cd7fdb8a42d01204e739bb862913
samantareinoso <samanta.reinoso@gmail.com>
2015-06-01 01:30:21 -0300

    Modificaciones en la tabla de usuario
    se modifico la tabla de usuario para que permitiera valores nulos en fecha
    fin y el pk sea autoincremental

--- AgenciaViajes/AgenciaDeViajesDB/dbo/Tables/Usuario_1.sql
+++ AgenciaViajes/AgenciaDeViajesDB/dbo/Tables/Usuario_1.sql
@@ -1,10 +1,16 @@
-﻿CREATE TABLE [dbo].[Usuario] (
-    [usuario]   INT   NOT NULL,
-    [activo]    BIT           NOT NULL,
-    [fechaAlta] DATETIME      NOT NULL,
-    [fechaBaja] DATETIME      NOT NULL,
-    [nombre]    NVARCHAR (50) NOT NULL,
-    [password]  NVARCHAR (8)  NOT NULL,
-    CONSTRAINT [PK_Usuario] PRIMARY KEY CLUSTERED ([usuario] ASC)
-);
+﻿
+
+CREATE TABLE [dbo].[Usuario](
+	[usuario] [int] IDENTITY(1,1) NOT NULL,
+	[activo] [bit] NOT NULL,
+	[fechaAlta] [datetime] NOT NULL,
+	[fechaBaja] [datetime] NULL,
+	[nombre] [nvarchar](50) NOT NULL,
+	[password] [nvarchar](8) NOT NULL,
+ CONSTRAINT [PK_Usuario] PRIMARY KEY CLUSTERED 
+(
+	[usuario] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY];
+
 
