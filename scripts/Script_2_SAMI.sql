fab19c7a98cb45d70b2387a602a4cdf3a24e43a7
consugus <consugus@gmail.com>
2015-06-03 22:22:42 -0300

    gustavo moquero

--- scripts/Script_2_SAMI.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[usp_Empleado_GetByTermino]
-	(@termino nvarchar(60))
-AS
-BEGIN
-	SET NOCOUNT ON;
-	SELECT * 
-	FROM Empleado
-	WHERE nombre like (@termino + '%') or apellido like (@termino + '%');
-END
